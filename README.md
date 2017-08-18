dfe02cd621e5a38bd45d9f5f8acb460241389986
Rodrigo Gomes da Silva <rodrigo.smscom@gmail.com>
2017-08-18 13:58:29 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# garrulous
+
+Convert any encoding to any encoding
+
+Install
+-------
+>```$ npm install garrulous -g```
+
+Usage
+-----
+
+```
+  Usage: garrulous [options]
+
+
+  Options:
+
+    -V, --version                      output the version number
+    -i, --inDirectory <inDirectory>    Input directory
+    -o, --outDirectory <outDirectory>  Output directory
+    -f, --fromEncode <fromEncode>      From encode
+    -t, --toEncode <toEncode>          To encode
+    -d, --debug                        Output debug information
+    -h, --help                         output usage information
+
+```
+
+Example
+-------
+>```$ garrulous -d -i /myfolder/in -o /myfolder/out -f UTF-8 -t ISO-8859-1```
+
+License
+-------
+[Licence](https://github.com/rodrigogs/garrulous/blob/master/LICENSE) © Rodrigo Gomes da Silva
---
+++ index.js
@@ -0,0 +1,30 @@
+const debug = require('debug')('garrulous:index');
+const Iconv  = require('iconv').Iconv;
+const fs = require('fs');
+const path = require('path');
+
+const createDir = (dir) => {
+  if (fs.existsSync(dir)) return;
+  fs.mkdirSync(dir);
+};
+
+module.exports = (options) => {
+  const { inDirectory, outDirectory, fromEncode, toEncode } = options;
+
+  const iconv = new Iconv(fromEncode, toEncode);
+
+  debug('creating directory', outDirectory);
+  createDir(outDirectory);
+
+  debug('looking for files');
+  const files = fs.readdirSync(inDirectory);
+  files.forEach((file) => {
+    debug('converting file', file);
+    const buffer = fs.readFileSync(path.join(inDirectory, file));
+    const convertered = iconv.convert(buffer);
+    debug('writing file', file);
+    fs.writeFileSync(path.join(outDirectory, file), convertered, { encoding: 'binary' });
+  });
+
+  debug('done');
+};
