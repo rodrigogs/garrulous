88809bbf21527ab11dbe24dbb3d05bd929753aeb
Rodrigo Gomes da Silva <rodrigo.smscom@gmail.com>
2017-08-18 19:04:49 -0300

    Added recursive option

--- index.js
+++ index.js
@@ -1,30 +1,53 @@
 const debug = require('debug')('garrulous:index');
-const Iconv  = require('iconv').Iconv;
 const fs = require('fs');
 const path = require('path');
+const Iconv = require('iconv').Iconv;
+const mkdirp = require('mkdirp');
+
+let iconv;
+
+const isDirectory = path => fs.statSync(path).isDirectory();
 
 const createDir = (dir) => {
   if (fs.existsSync(dir)) return;
-  fs.mkdirSync(dir);
+  mkdirp(dir);
 };
 
-module.exports = (options) => {
-  const { inDirectory, outDirectory, fromEncode, toEncode } = options;
-
-  const iconv = new Iconv(fromEncode, toEncode);
+const convert = (inDirectory, outDirectory, fromEncode, toEncode, recursive) => {
+  iconv = new Iconv(fromEncode, `${toEncode}//TRANSLIT`);
 
   debug('creating directory', outDirectory);
   createDir(outDirectory);
 
   debug('looking for files');
+
+  if (!isDirectory(inDirectory)) throw new Error('Input is not a directory');
+
   const files = fs.readdirSync(inDirectory);
   files.forEach((file) => {
+    const filePath = path.join(inDirectory, file);
+
+    if (isDirectory(filePath)) {
+      debug('path', filePath, 'is a directory');
+      if (recursive) {
+        debug('converting', filePath, 'files recursively');
+        const out = path.join(outDirectory, file);
+        return convert(filePath, out, fromEncode, toEncode, recursive);
+      }
+      return;
+    }
+
     debug('converting file', file);
-    const buffer = fs.readFileSync(path.join(inDirectory, file), { encoding: fromEncode });
+    const buffer = fs.readFileSync(filePath, { encoding: fromEncode });
     const convertered = iconv.convert(buffer);
     debug('writing file', file);
     fs.writeFileSync(path.join(outDirectory, file), convertered, { encoding: 'binary' });
   });
 
-  debug('done');
+  debug('finished directory', inDirectory);
+};
+
+module.exports = (options) => {
+  const { inDirectory, outDirectory, fromEncode, toEncode, recursive } = options;
+  convert(inDirectory, outDirectory, fromEncode, toEncode, recursive);
 };
